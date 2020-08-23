ebe55ab43e860df01192c17821a53b3f1e96434f
brokiem <61258338+brokiem@users.noreply.github.com>
2020-08-23 12:52:33 +0800

    Update Board.php

--- src/brokiem/QuickBoard/Board.php
+++ src/brokiem/QuickBoard/Board.php
@@ -25,7 +25,7 @@ class Board extends PluginTask {
                     $api->new($p, "Title", "§l§bLOBBY");
                     $api->setLine($p, 1, " ");
                     $api->setLine($p, 2, "§7Hi, §3". $p->getName());
-                    $api->setLine($p, 3, "§7Coin: §3". EconomyAPI::getInstance()->myMoney($p);
+                    $api->setLine($p, 3, "§7Coin: §3". EconomyAPI::getInstance()->myMoney($p));
                     $api->setLine($p, 4, "    ");
                     $api->setLine($p, 5, "§7Lobby §3#1");
                     $api->setLine($p, 6, "§7Online: §3". $this->plugin->getServer()->getOnlinePlayers());
