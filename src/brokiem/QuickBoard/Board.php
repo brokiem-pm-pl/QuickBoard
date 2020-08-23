0fae4544f77552c768310fe3320f2a7a0f2cf629
brokiem <61258338+brokiem@users.noreply.github.com>
2020-08-23 12:07:00 +0800

    Update Board.php

--- src/brokiem/QuickBoard/Board.php
+++ src/brokiem/QuickBoard/Board.php
@@ -19,13 +19,6 @@ class Board extends PluginTask {
     public function onRun(int $tick) : void
     {
         $main = $this->plugin;
-        
-        try{
-		        $server = PMQuery::query("104.215.250.131", 11001);
-				$i = $server['Players'];
-			}catch(PmQueryException $e){
-				$i = "§cOFF";
-			}
       
         foreach ($this->getPlayers() as $p) {
                     $api = Scoreboards::getInstance();
@@ -35,7 +28,7 @@ class Board extends PluginTask {
                     $api->setLine($p, 3, "§7Coin: §3". EconomyAPI::getInstance()->myMoney($p);
                     $api->setLine($p, 4, "    ");
                     $api->setLine($p, 5, "§7Lobby §3#1");
-                    $api->setLine($p, 6, "§7Online: §3". $i);
+                    $api->setLine($p, 6, "§7Online: §3". $this->plugin->getServer()->getOnlinePlayers());
                     $api->setLine($p, 7, "      ");
                     $api->setLine($p, 8, "§3kawaismp.net");
                     $api->getObjectiveName($p);
