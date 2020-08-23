903cc9c39c676cadd3480ddfa60ddb36b52d9169
brokiem <61258338+brokiem@users.noreply.github.com>
2020-08-23 12:29:09 +0800

    Update MainBoard.php

--- src/brokiem/QuickBoard/MainBoard.php
+++ src/brokiem/QuickBoard/MainBoard.php
@@ -4,12 +4,12 @@ namespace brokiem\QuickBoard;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\Task;
+use pocketmine\utils\Config;
 
 class MainBoard extends PluginBase 
 {
-
     public function onEnable()
     {
-        $this->getScheduler()->scheduleRepeatingTask(new Board($this), 40); //2 seconds
+        $this->getScheduler()->scheduleRepeatingTask(new Board($this), (int) $this->getConfig()->get("refresh") * 20);
     }
 }
