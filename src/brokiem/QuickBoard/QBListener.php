7a6e22f90fc537594afff7648b8eb580aa2e5ca1
brokiem <61258338+brokiem@users.noreply.github.com>
2020-08-24 12:22:20 +0800

    Update QBListener.php

--- src/brokiem/QuickBoard/QBListener.php
+++ src/brokiem/QuickBoard/QBListener.php
@@ -23,19 +23,18 @@ class QBListener implements Listener {
     
     public function Holders(Player $player, string $holder): string
     {
+	$level = $player->getLevel();
         if($this->plugin->getConfig()->get("enable") === true){
-		    try{
-		        $server = PMQuery::query($this->plugin->getConfig()->get("ip"), ($this->plugin->getConfig()->get("port")));
-	            $total = $server['Players'];
-		    }catch(PmQueryException $e){
-		        $total = "§cOFFLINE";
-		    }
-		}elseif($this->plugin->getConfig()->get("enable") === false){
-			$total = "Disabled";
-		}
-        
-        //$holder = $this->plugin->getConfig()->get("quickboard-lines");
-        $holder = str_replace("{name}", $player->getName(), $holder);
+	    try{
+		$server = PMQuery::query($this->plugin->getConfig()->get("ip"), ($this->plugin->getConfig()->get("port")));
+	        $total = $server['Players'];
+  	    }catch(PmQueryException $e){
+		$total = "§cOFFLINE";
+	    }
+	    }elseif($this->plugin->getConfig()->get("enable") === false){
+			$total = "§cDisabled";
+	    }
+        $holder = str_replace("%name%", $player->getName(), $holder);
         $holder = str_replace("%display_name%", $player->getDisplayName(), $holder);
         $holder = str_replace("%name%", $player->getName(), $holder);
         $holder = str_replace("%server_online%", count($player->getServer()->getOnlinePlayers()), $holder);
@@ -43,7 +42,15 @@ class QBListener implements Listener {
         $holder = str_replace("%server_tps%", $player->getServer()->getTicksPerSecond(), $holder);
         $holder = str_replace("%player_ping%", $player->getPing(), $holder);
         $holder = str_replace("%server_load%", $player->getServer()->getTickUsage(), $holder);
-		$holder = str_replace("%server_query%", $total, $holder);
+	$holder = str_replace("%server_query%", $total, $holder);
+        $holder = str_replace("%item_id%", $player->getInventory()->getItemInHand()->getId(), $holder);
+	    $holder = str_replace("%player_ip%", $player->getAddress(), $holder);
+	    $holder = str_replace("%player_x%", $player->getFloorX(), $holder);
+	    $holder = str_replace("%player_y%", $player->getFloorY(), $holder);
+	    $holder = str_replace("%player_z%", $player->getFloorZ(), $holder);
+	    $holder = str_replace("%player_world%", $level->getFolderName(), $holder);
+	    $holder = str_replace("%world_player_count%", count($level->getPlayers()), $holder);
+	    $holder = str_replace("%date%", date("H:i a"), $holder);
 		return ((string) $holder);
     }
 
