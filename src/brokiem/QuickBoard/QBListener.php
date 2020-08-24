cd7c8c1f2eca0ab7520fa0ea5a038a5373c81235
brokiem <61258338+brokiem@users.noreply.github.com>
2020-08-24 09:33:47 +0800

    Update QBListener.php

--- src/brokiem/QuickBoard/QBListener.php
+++ src/brokiem/QuickBoard/QBListener.php
@@ -2,33 +2,48 @@
 
 namespace brokiem\QuickBoard;
 
-use pocketmine\scheduler\Task;
 use pocketmine\Player;
 use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
 use pocketmine\utils\Config;
 
-class BoardTask extends Task {
+use brokiem\QuickBoard\libs\libpmquery\PMQuery;
+use brokiem\QuickBoard\libs\libpmquery\PmQueryException;
 
-    public function __construct(MainBoard $plugin)
+class QBListener implements Listener {
+    
+	private $plugin;
+	
+   public function __construct(MainBoard $plugin)
     {
         $this->plugin = $plugin;
     }
     
-    public function onRun(int $tick)
+    public function Holders(Player $player)
     {
-        $main = $this->plugin;
-		$qbline = $this->plugin->getConfig()->get("quickboard-lines");
-      
-        foreach ($this->plugin->getServer()->getOnlinePlayers() as $p) {
-                $main->new($p, "Title", ($this->plugin->getConfig()->get("quickboard-title")));
-                $c = 0;
-                foreach ((array)$qbline as $lines){
-                    $c++;
-                    if($c <= 15){
-                        $main->setLine($p, $c, $lines);
-                    }
-                }
-                $main->getObjectiveName($p);
-        }
-    }
+        if($this->plugin->getConfig()->get("enable") === true){
+		try{
+		    $server = PMQuery::query($this->plugin->getConfig()->get("ip"), ($this->plugin->getConfig()->get("port")));
+	            $total = $server['Players'];
+	            Server::getInstance()->getLogger()->info("QuickBoard> There are ".$players." on the queried server right now!");
+		}catch(PmQueryException $e){
+		    $total = "§cOFFLINE";
+		    Server::getInstance()->getLogger()->info("QuickBoard> The queried server is offline right now!");
+		} 
+		}
+        
+        $config = $this->getConfig()->get("quickboard-lines");
+        $holder = str_replace("%name%", $player->getName(), $config);
+        $holder = str_replace("%display_name%", $player->getDisplayName(), $config);
+        $holder = str_replace("%name%", $player->getName(), $config);
+        $holder = str_replace("%server_online%", count($player->getServer()->getOnlinePlayers()), $config);
+        $holder = str_replace("%max_online%", $player->getServer()->getMaxPlayers(), $config);
+        $holder = str_replace("%server_tps%", $player->getServer()->getTicksPerSecond(), $config);
+        $holder = str_replace("%player_ping%", $player->getPing(), $config);
+        $holder = str_replace("%server_load%", $player->getServer()->getTickUsage(), $config);
+        $holder = str_replace("%server_query%", $total, $config);
+        // more holders soon :D
+     }
+
 }
